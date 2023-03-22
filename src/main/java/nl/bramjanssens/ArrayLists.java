0e831ddec681389eea5d703f482068c0054b4317
Bram Janssens <s.a.janssens@gmail.com>
2023-03-22 15:18:32 +0100

    ArrayLists modified.

--- src/main/java/nl/bramjanssens/ArrayLists.java
+++ src/main/java/nl/bramjanssens/ArrayLists.java
@@ -3,7 +3,21 @@ package nl.bramjanssens;
 import java.util.ArrayList;
 
 public class ArrayLists {
+
     public static void main(String[] args) {
+        // met arrays:
+        int currentElement = 0;
+        int[] getallenAlsArray = new int[10];
+        getallenAlsArray[currentElement] = 42;
+        currentElement++;
+        getallenAlsArray[currentElement] = 43;
+        currentElement++;
+
+        for (int i = 0; i < currentElement; i++) {
+            System.out.println(getallenAlsArray[i]);
+        }
+
+        // same met arraylist:
         ArrayList<Integer> getallen = new ArrayList<Integer>();
 
         getallen.add(1);
@@ -11,21 +25,10 @@ public class ArrayLists {
         getallen.add(-23341);
         getallen.add(10);
 
-        System.out.println(getallen.get(0));
+        System.out.println(getallen.remove(Integer.valueOf(1)));
 
         for (Integer item : getallen) {
             System.out.println(item);
-        }
-
-        // met arrays:
-        int currentElement = 0;
-        int[] getallenAlsArray = new int[10];
-        getallenAlsArray[currentElement] = 42;
-        currentElement++;
-        getallenAlsArray[currentElement] = 42;
-        currentElement++;
-        for (int i = 0; i < currentElement; i++) {
-            System.out.println(getallenAlsArray[i]);
         }
     }
 }
