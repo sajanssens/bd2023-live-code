a5ea2e242b376ddee3c6f4734cbc68262763a92e
Bram Janssens <s.a.janssens@gmail.com>
2023-03-29 15:26:23 +0200

    Some maven stuff.

---
+++ src/test/java/nl/bramjanssens/SomeIntegrationTestIT.java
@@ -0,0 +1,11 @@
+package nl.bramjanssens;
+
+import org.junit.jupiter.api.Test;
+
+class SomeIntegrationTestIT {
+
+    @Test
+    void name() {
+        System.out.println("--------------------------- IT works! ---------------------------");
+    }
+}
