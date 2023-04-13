461f2366786ff4017e186080bdd043a6da5bf698
Bram Janssens <s.a.janssens@gmail.com>
2023-04-13 11:38:32 +0200

    Hashing.

--- blok1/src/main/java/nl/bramjanssens/hashing/Hashing.java
+++ blok1/src/main/java/nl/bramjanssens/hashing/Hashing.java
@@ -1,19 +1,35 @@
 package nl.bramjanssens.hashing;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class Hashing {
+
     public static void main(String[] args) {
+        Person bram0 = new Person(42, "Bram");
+        Person bram1 = new Person(43, "Bram"); // equal by name, but also by hashcode?
+        Person bram2 = new Person(43, "Mieke");
+
         Set<Person> persons = new HashSet<>();
-        Person bram = new Person(42, "Bram");
-        Person bram1 = new Person(43, "Bram");
 
-        persons.add(bram);
+        persons.add(bram0);
         persons.add(bram1);
+        persons.add(bram2);
 
-        if (bram.equals(bram1)) System.out.println("Equals");
-        System.out.println(persons.size());
+        if (bram0.equals(bram1)) System.out.println("Equals");
+        persons.forEach(System.out::println);
+
+        // A hashset uses a map internally to store its elements as keys in the map, like this:
+        Map<Person, Object> personMap = new HashMap<>();
+        Object PRESENT = new Object();
+
+        personMap.put(bram0, PRESENT);
+        personMap.put(bram1, PRESENT); // key must be unique, so equal persons won't get put into map
+        personMap.put(bram2, PRESENT);
+
+        personMap.keySet().forEach(System.out::println);
     }
 }
 
@@ -28,11 +44,18 @@ class Person {
 
     @Override
     public int hashCode() {
-        return 42;// age ;
+        return 42;
     }
 
     @Override
     public boolean equals(Object obj) {
         return name.equals(((Person) obj).name);
+    }
+
+    @Override public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
