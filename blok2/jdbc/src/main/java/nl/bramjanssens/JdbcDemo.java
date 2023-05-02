a34fea7e49ae525aba609baa65353cb1f59af800
Bram Janssens <s.a.janssens@gmail.com>
2023-05-02 15:33:34 +0200

    resultsetmetadata

--- blok2/jdbc/src/main/java/nl/bramjanssens/JdbcDemo.java
+++ blok2/jdbc/src/main/java/nl/bramjanssens/JdbcDemo.java
@@ -3,8 +3,12 @@ package nl.bramjanssens;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
 
 public class JdbcDemo {
 
@@ -13,24 +17,26 @@ public class JdbcDemo {
         // 1
         // Class.forName("com.mysql.cj.jdbc.Driver"); // SPI
 
-        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo", "root", "root")){
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcdemo", "root", "root")) {
             // 3
             Statement statement = connection.createStatement();
 
             // 4
-            ResultSet result = statement.executeQuery("SELECT * FROM PERSON");
+            ResultSet result = statement.executeQuery("SELECT * FROM person");
 
-            // List<String> columns = new ArrayList<>();
-            // ResultSetMetaData metaData = result.getMetaData();
-            // for (int i = 1; i <= metaData.getColumnCount(); i++) {
-            //     columns.add(metaData.getColumnName(i));
-            // }
+            List<String> columns = new ArrayList<>();
+            ResultSetMetaData metaData = result.getMetaData();
+            for (int i = 1; i <= metaData.getColumnCount(); i++) {
+                columns.add(metaData.getColumnName(i));
+            }
 
             while (result.next()) {
-                String name = result.getString("name");
-                int age = result.getInt("age");
-
-                System.out.println(name + " - " + age);
+                StringJoiner sj = new StringJoiner(" | ");
+                for (String column : columns) {
+                    String value = result.getString(column);
+                    sj.add(value);
+                }
+                System.out.println(sj);
             }
         }
 
