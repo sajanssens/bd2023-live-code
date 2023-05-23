63a1252836ba8db0ed3204bd99c4e0d476578cf0
Bram Janssens <s.a.janssens@gmail.com>
2023-05-23 12:19:19 +0200

    HelloWorld ts added.

---
+++ frontend/blok3/ts/HelloWorld.ts
@@ -0,0 +1,7 @@
+class HelloWorld {
+    static sayHello(m: string): void {
+        let data: string = 'Hello world';
+        console.log(data);
+    }
+}
+HelloWorld.sayHello('Bram');
--- frontend/blok3/ts/test.ts
+++
@@ -1,2 +0,0 @@
-let x: number = 10
-console.log(x)
