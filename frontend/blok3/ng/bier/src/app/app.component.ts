9bae51cf8719d88bfb253d76f2c15f4aedcfde05
Bram Janssens <s.a.janssens@gmail.com>
2023-05-24 13:59:21 +0200

    More core demo's.

--- frontend/blok3/ng/bier/src/app/app.component.ts
+++ frontend/blok3/ng/bier/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({ // annotation = decorator in TS
   selector: 'app-root',
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'bier';
+  isVisible = false;
+  inputValue = 'tiep hier iets...'
+
+  cars = [
+    {make: 'Opel', model: 'Astra', price: 29995},
+    {make: 'Porsche', model: '911', price: 145000},
+    {make: 'Fiat', model: 'Uno', price: 1800}
+  ];
+
+  buttonId = 'veryImportantButton'
+
+  toggle() {
+    this.isVisible = !this.isVisible
+  }
+
+  resetInputValue() {
+    this.inputValue = ''
+  }
 }
--- frontend/blok3/ng/bier/src/app/app.module.ts
+++ frontend/blok3/ng/bier/src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SubtitleComponent } from './components/subtitle/subtitle.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SubtitleComponent} from './components/subtitle/subtitle.component';
+import {FormsModule} from "@angular/forms";
+import {TwoWayDataBindingComponent} from './components/two-way-data-binding/two-way-data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SubtitleComponent
+    SubtitleComponent,
+    TwoWayDataBindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
