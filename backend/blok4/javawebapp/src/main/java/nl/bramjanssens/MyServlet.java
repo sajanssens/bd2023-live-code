5632aa9fe92edf6cefcd41bd86a1137aaa43eab5
Bram Janssens <s.a.janssens@gmail.com>
2023-06-01 15:04:43 +0200

    blok4/javawebapp added.

---
+++ backend/blok4/javawebapp/src/main/java/nl/bramjanssens/MyServlet.java
@@ -0,0 +1,25 @@
+package nl.bramjanssens;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String q = req.getParameter("q");
+        resp.getWriter().print("<html><body><h1>Hello world " + q + "!</h1></body></html>");
+        resp.setStatus(218);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
