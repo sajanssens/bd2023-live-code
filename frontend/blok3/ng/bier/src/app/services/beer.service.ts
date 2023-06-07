be745637445ae1b61adc5150953ee0bd234b3ded
Bram Janssens <s.a.janssens@gmail.com>
2023-06-07 10:32:59 +0200

    Refactored service: environment now.

--- frontend/blok3/ng/bier/src/app/services/beer.service.ts
+++ frontend/blok3/ng/bier/src/app/services/beer.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Beer} from "../components/model/Beer";
 import {HttpClient} from "@angular/common/http";
-import {Subject} from "rxjs";
+import {Observable, Subject} from "rxjs";
+import {serverUrl} from "../../environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class BeerService {
@@ -15,36 +16,38 @@ export class BeerService {
   constructor(private http: HttpClient) {
   }
 
+  private url = `${serverUrl}/beers`;
+
   getAll(): void {
-    this.http.get<Beer[]>('http://localhost:3000/beers').subscribe(
+    this.http.get<Beer[]>(this.url).subscribe(
       (beersFromBackend) => this._beersAreUpdated$.next(beersFromBackend)
     )
   }
 
-  add(c: Beer): void {
-    this.http.post<Beer>('http://localhost:3000/beers', c).subscribe(
+  add(b: Beer): void {
+    this.http.post<Beer>(this.url, b).subscribe(
       () => this.getAll()
     )
   }
 
   delete(id?: number): void {
-    this.http.delete<void>('http://localhost:3000/beers/' + id).subscribe(
+    this.http.delete<void>(`${this.url}/${id}`).subscribe(
       () => this.getAll()
     )
   }
 
-  search(searchterm: string): void {
-    this.http.get<Beer[]>('http://localhost:3000/beers?q=' + searchterm).subscribe(
+  search(term: string): void {
+    this.http.get<Beer[]>(`${this.url}?q=${term}`).subscribe(
       (beersFromBackend) => this._beersAreUpdated$.next(beersFromBackend)
     )
   }
 
-  get(id: number) {
-    return this.http.get<Beer>(`http://localhost:3000/beers/${id}`)
+  get(id: number): Observable<Beer> {
+    return this.http.get<Beer>(`${this.url}/${id}`)
   }
 
-  update(beer: Beer) {
-    return this.http.patch<Beer>(`http://localhost:3000/beers/${beer.id}`, beer)
+  update(b: Beer): void {
+    this.http.patch<Beer>(`${this.url}/${b.id}`, b)
       .subscribe(() => this.getAll());
   }
 }
