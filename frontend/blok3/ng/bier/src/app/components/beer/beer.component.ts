adf903c91746dfa47bc75375a65847f886455dbd
Bram Janssens <s.a.janssens@gmail.com>
2023-06-26 15:47:28 +0200

    Added bootstrap vaildation to "add beer".

--- frontend/blok3/ng/bier/src/app/components/beer/beer.component.ts
+++ frontend/blok3/ng/bier/src/app/components/beer/beer.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {Beer} from "../model/Beer";
 import {BeerService} from "../../services/beer.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-beer-details',
@@ -12,6 +13,7 @@ export class BeerComponent {
   beer: Beer = {} as Beer
   show: boolean = false;
   private editMode = false;
+  formClass: string = 'needs-validation';
 
   constructor(private beerService: BeerService, private route: ActivatedRoute, private router: Router) {
     if (this.route.snapshot.paramMap.has('id')) {
@@ -22,7 +24,10 @@ export class BeerComponent {
     }
   }
 
-  save() {
+  save(beerForm: NgForm) {
+    this.formClass = 'was-validated'
+    if (!beerForm.valid) return
+
     if (this.editMode) {
       this.beerService.update(this.beer)
       this.router.navigate(['beers'])
