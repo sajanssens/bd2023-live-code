6954f97f4b98d097402a6c2bd3abce55bdb0e232
Bram Janssens <s.a.janssens@gmail.com>
2024-06-18 16:09:41 +0200

    Gson test improved.

--- backend/blok4/rest/src/test/java/nl/belastingdienst/GsonTest.java
+++ backend/blok4/rest/src/test/java/nl/belastingdienst/GsonTest.java
@@ -2,28 +2,27 @@ package nl.belastingdienst;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 import org.junit.jupiter.api.Test;
 
 import java.io.Serializable;
-import java.lang.reflect.Type;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GsonTest {
 
     @Test
     void gson() {
         Gson gson = new GsonBuilder()
-                // .setPrettyPrinting()
                 .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
                 .create();
 
-        System.out.println(gson.toJson(new Person("Bram", LocalDate.now())));
+        LocalDate now = LocalDate.now();
+        String json = gson.toJson(new Person("Bram", now));
+
+        assertTrue(json.contains(now.toString()));
     }
 }
-record Person(String name, LocalDate birthday) implements Serializable {}
+
+record Person(String name, LocalDate birthday) implements Serializable { }
 
